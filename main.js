7eb7d822767643fc4d56e28a66e6725b10646706
Caroline <wanguic49@gmail.com>
2023-07-29 23:00:32 +0300

    validation for email format

--- main.js
+++ main.js
@@ -2,12 +2,23 @@ function validateForm(){
     let name = document.subForm.name.value;
     let password = document.subForm.myPassword.value;
     let num = document.subForm.num.value;
+    let x = document.subForm.email.value;
+    let atPosition = x.indexOf("@");
+    let dotPosition = x.lastIndexOf(".");
+
 
     if (name == null || name === "") {
         alert("Name cannot be empty");
         return false;
     } else if (password.length < 6) {
         alert("Password must be at least 6 characters long");
+        return false;
+    }
+
+    if(atPosition < 1 || dotPosition < atPosition + 2  || dotPosition+2 >x.length ){
+        alert("Please enter a valid e-mail address \n atPosition:" + atPosition + "\n dotPosition:"
+        +dotPosition
+        );
         return false;
     }
 
@@ -22,4 +33,8 @@ function validateForm(){
         document.subForm.submit();
         return true;
     }
+
+    
+
+    
 }
